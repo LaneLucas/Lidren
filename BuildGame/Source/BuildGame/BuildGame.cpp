6e17b8aae8b808abe386ae165162d06f6d74fdf8
LaneLucas <lanelucas.612@gmail.com>
2022-03-19 17:06:39 -0400

    added code structure

---
+++ BuildGame/Source/BuildGame/BuildGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuildGame, "BuildGame" );
